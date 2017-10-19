379c8655303ded4f53104571481fdf9ea7de224c
Johannes Raggam <thetetet@gmail.com>
2017-10-19 12:48:04 +0200

    use local again - offline doesn't work

--- resources/init.js
+++ resources/init.js
@@ -3,8 +3,8 @@
 (function (Reveal, $) {
   "use strict";
 
-  var base_url = 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.5.0';
-  //  var base_url = '/resources/lib/reveal.js';
+  // var base_url = 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.5.0';
+  var base_url = '/resources/libs/reveal.js';
 
   // Full list of configuration options available here:
   // https://github.com/hakimel/reveal.js#configuration
@@ -27,7 +27,7 @@
 
     // Optional libraries used to extend on reveal.js
     dependencies: [
-      { src: '/resources/lib/reveal.js-plugin-externalcode/externalcode.js', condition: function() { return !!document.querySelector( '[data-code]' ); } },
+      { src: '/resources/libs/reveal.js-plugin-externalcode/externalcode.js', condition: function() { return !!document.querySelector( '[data-code]' ); } },
       { src: base_url + '/lib/js/classList.js', condition: function() { return !document.body.classList; } },
       { src: base_url + '/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
       { src: base_url + '/plugin/zoom-js/zoom.js', condition: function() { return !!document.body.classList; } },
